367474044526a98b03da72c91415f6b2b24dccc2
Brad Caron <bacaron@iu.edu>
2021-07-06 17:08:28 -0400

    reason: update to include affine

--- reslice.sh
+++ reslice.sh
@@ -2,6 +2,8 @@
 
 rois=`jq -r '.rois' config.json`
 anat=`jq -r '.anat' config.json`
+affine=`jq -r '.affine' config.json`
+inverse=`jq -r 'inverse' config.json`
 
 set -x
 
@@ -10,9 +12,16 @@ set -x
 
 [ -z "$(ls -A ./rois/)" ] && echo "copying rois" && cp -R ${rois} ./rois/
 
-# reslice rois
-echo "reslicing rois"
-for i in `ls ./rois/rois/`
+# compute inverse affine
+if [[ ${inverse} == 'true' ]]; then
+	echo "computing inverse affine"
+	convert_xfm -omat inverse-affine.txt -inverse affine.txt
+	affine='inverse-affine.txt'
+fi
+
+# apply affine to all ROIs to get back to subject space
+echo "moving ROIs to anatomy space"
+for i in `ls ./rois/rois`
 do
-	mri_vol2vol --mov ./rois/rois/${i} --targ ${anat} --regheader --interp nearest --o ./output/rois/${i}
+	flirt -in ./rois/rois/${i} --ref ${anat} --applyxfm -init inverse-affine.txt -interp nearestneighbour --out ./output/rois/${i}
 done
