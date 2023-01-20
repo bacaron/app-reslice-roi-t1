01b5ca27e4215a07be6e7b989c8d2b5ec844ea51
Brad Caron <bacaron245@gmail.com>
2023-01-20 15:10:35 -0600

    Update reslice.sh

--- reslice.sh
+++ reslice.sh
@@ -23,6 +23,6 @@ fi
 echo "moving ROIs to anatomy space"
 for i in `ls ./rois/rois`
 do
-	flirt -in ./rois/rois/${i} -ref ${anat} -applyxfm -init inverse-affine.txt -interp nearestneighbour -out ./output/rois/${i}
+	flirt -in ./rois/rois/${i} -ref ${anat} -applyxfm -init ${affine} -interp nearestneighbour -out ./output/rois/${i}
 	fslmaths ./output/rois/${i} -bin ./output/rois/${i}
 done
